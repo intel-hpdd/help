39e2ceb42e038d4145388fcbcf4c833266691bcc
Joe Grund <grundjoseph@gmail.com>
2017-04-24 15:40:55 -0400

    Update References to mfl
    Fixes #7.
    
    Update all references from IML to MFL.
    
    Also, add travis to repo.
    
    Update references.
    Add travis.
    
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

--- run_test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# There are no tests to run, but CI needs a results file.
-echo '<testsuites><testsuite name="help" package="help" timestamp="2016-07-08" id="0" hostname="localhost" tests="0" errors="0" failures="0" skipped="0" time="0.008"></testsuite></testsuites>' > help-results.xml
-mv help-results.xml ../results
